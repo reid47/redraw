964cda8bcd12604b56804bbabfad61981772ab6b
Reid Mitchell <reidmitchell1@gmail.com>
2017-12-30 01:37:07 -0500

    Fixing selection

--- src/actions/select.js
+++ src/actions/select.js
@@ -19,28 +19,44 @@ export const select = {
       ghostCtx.fillStyle = 'rgba(0, 0, 0, 0.7)';
       ghostCtx.fillRect(0, 0, ghostCtx.canvas.width, ghostCtx.canvas.height);
 
-      const startX = Math.min(this.canvasSelectStartX, x);
-      const startY = Math.min(this.canvasSelectStartY, y);
       const selectW = Math.abs(this.canvasSelectStartX - x);
       const selectH = Math.abs(this.canvasSelectStartY - y);
-      const w = !evt.shiftKey ? selectW : Math.max(selectW, selectH);
-      const h = !evt.shiftKey ? selectH : Math.max(selectW, selectH);
+
+      let startX, startY, w, h;
+      if (!evt.shiftKey) {
+        startX = Math.min(this.canvasSelectStartX, x);
+        startY = Math.min(this.canvasSelectStartY, y);
+        w = selectW;
+        h = selectH;
+      } else {
+        const size = Math.max(selectW, selectH);
+        startX = x < this.canvasSelectStartX
+          ? this.canvasSelectStartX - size
+          : this.canvasSelectStartX;
+        startY = y < this.canvasSelectStartY
+          ? this.canvasSelectStartY - size
+          : this.canvasSelectStartY;
+        w = size;
+        h = size;
+      }
 
       this.setState({
         selectionActive: true,
+        selectionStartX: startX,
+        selectionStartY: startY,
         selectionWidth: w,
         selectionHeight: h
       });
 
-      ghostCtx.strokeStyle = 'white';
-      ghostCtx.beginPath();
-      ghostCtx.moveTo(startX, startY);
-      ghostCtx.lineTo(startX + w, startY);
-      ghostCtx.lineTo(startX + w, startY + h);
-      ghostCtx.lineTo(startX, startY + h);
-      ghostCtx.lineTo(startX, startY);
-      ghostCtx.closePath();
-      ghostCtx.stroke();
+      // ghostCtx.strokeStyle = 'white';
+      // ghostCtx.beginPath();
+      // ghostCtx.moveTo(startX, startY);
+      // ghostCtx.lineTo(startX + w, startY);
+      // ghostCtx.lineTo(startX + w, startY + h);
+      // ghostCtx.lineTo(startX, startY + h);
+      // ghostCtx.lineTo(startX, startY);
+      // ghostCtx.closePath();
+      // ghostCtx.stroke();
 
       ghostCtx.clearRect(startX, startY, w, h);
     }
